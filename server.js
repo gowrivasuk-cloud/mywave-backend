490c80be36145af49c24db3d209adce0273a03d9
Gowri <gowrivasuk@gmail.com>
2026-06-02 12:44:54 +0530

    fix render port

--- server.js
+++ server.js
@@ -571,10 +571,8 @@ app.get(
    START SERVER
 ===================================================== */
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
 
-  console.log(
-    "🚀 Server running on http://localhost:5000"
-  );
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
